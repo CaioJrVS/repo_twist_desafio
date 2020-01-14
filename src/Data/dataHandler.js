5a8f9556bdce02602332c821cf2bc6971f7995f2
CaioJrVS <caio.junior.vs@gmail.com>
2020-01-14 15:25:18 -0300

    Task: algorithm that handles the data

--- src/Data/dataHandler.js
+++ src/Data/dataHandler.js
@@ -1,11 +1,11 @@
 const data = require('./data');
 
-//Votos de cada candidato:
+//Noticias de cada candidato:
 
 let noticiasPorCandidato = [];
 let noticiasFreixo = 0;
 let noticiasCrivella = 0;
-data.data.map((value, index)=>{
+data.data.map(value=>{
     if (value.candidato == 'Crivella'){
         noticiasCrivella ++;
     }
@@ -14,6 +14,45 @@ data.data.map((value, index)=>{
     }
 });
 
-noticiasPorCandidato.push({noticiasCrivela:noticiasCrivella}, {noticiasFreixo:noticiasFreixo});
+noticiasPorCandidato= {Crivella:noticiasCrivella, Freixo:noticiasFreixo};
 
 exports.noticiasPorCandidato= noticiasPorCandidato;
+
+
+//Noticias de cada candidato por dia
+
+
+let noticiasPorDiaCrivella =[];
+let noticiasPorDiaFreixo =[];
+
+contaDias =0;
+dia = data.data[0].date_published.slice(8,10);
+for (i=0; i< noticiasPorCandidato.Freixo ;i++){
+
+    if (data.data[i].date_published.slice(8,10) == dia){
+        contaDias++
+    }
+    else{
+        noticiasPorDiaFreixo.push({dia:data.data[i].date_published.slice(5,10),numDeNoticias:contaDias});
+        contaDias=1;
+        dia=data.data[i].date_published.slice(8,10);
+    }
+};
+noticiasPorDiaFreixo.push({dia:data.data[i].date_published.slice(5,10),numDeNoticias:contaDias});
+
+contaDias =0
+dia = data.data[noticiasPorCandidato.Freixo].date_published.slice(8,10);
+
+for( i = noticiasPorCandidato.Freixo; i< noticiasPorCandidato.Crivella+noticiasPorCandidato.Freixo; i++ ){
+    if (data.data[i].date_published.slice(8,10) == dia){
+        contaDias++
+    }
+    else{
+        noticiasPorDiaCrivella.push({dia:data.data[i].date_published.slice(5,10),numDeNoticias:contaDias});
+        contaDias=1;
+        dia=data.data[i].date_published.slice(8,10);
+    }
+};
+noticiasPorDiaCrivella.push(contaDias);
+
+exports.noticiasPorDia= {Crivella:noticiasPorDiaCrivella,Freixo:noticiasPorDiaFreixo};
